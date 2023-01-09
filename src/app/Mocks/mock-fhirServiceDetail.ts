3465b6c04dc3c8fb04d6eeb04abaee32735c7a0e
Quentin H <huet-quentin@hotmail.fr>
2023-01-09 10:12:31 +0100

    mock fhir service detail complet

--- src/app/Mocks/mock-fhirServiceDetail.ts
+++ src/app/Mocks/mock-fhirServiceDetail.ts
@@ -66,4 +66,84 @@ export const FHIRServiceDetail: FhirServiceDetailInterface[] = [
         type: "String",
         label: "Filtre sur le nom et le prénom du patient",
     },
+    {
+        name: "author.practitioner.name",
+        type: "String",
+        label: "Filtre sur le nom et le prénom de l'auteur (professionnel)",
+    },
+    {
+        name: "family",
+        type: "String",
+        label: "Filtre sur le nom du patient",
+    },
+    {
+        name: "author.PractitionerRole.partOf.family-ex",
+        type: "String",
+        label: "Filtre sur le nom de l'auteur (professionnel)",
+    },
+    {
+        name: "given",
+        type: "String",
+        label: "Filtre sur le prénom du patient",
+    },
+    {
+        name: "author.PractitionerRole.partOf.given-ex",
+        type: "String",
+        label: "Filtre sur le prénom de l'auteur (professionnel)",
+    },
+    {
+        name: "nature",
+        type: "String",
+        label: "Filtre sur la nature du séjour",
+    },
+    {
+        name: "nature.idcegi",
+        type: "Number",
+        label: "Filtre sur l'identifiant interne de la nature",
+    },
+    {
+        name: "categorie.code",
+        type: "String",
+        label: "Filtre sur le code la catégorie de document",
+    },
+    {
+        name: "categorie.idcegi",
+        type: "Number",
+        label: "Filtre sur l'identifiant interne de la catégorie du document",
+    },
+    {
+        name: "famille.code",
+        type: "String",
+        label: "Filtre sur le code de la famille de document",
+    },
+    {
+        name: "famille.idcegi",
+        type: "Number",
+        label: "Filtre sur l'identifiant interne de la famille du document",
+    },
+    {
+        name: "modele.code",
+        type: "String",
+        label: "Filtre sur le code du modèle de document",
+    },
+    {
+        name: "modele.idcegi",
+        type: "Number",
+        label: "Filtre sur l'identifiant interne du modèle de document",
+    },
+    {
+        name: "type",
+        type: "Token",
+        label: "Type du document",
+    },
+    {
+        name: "facility",
+        type: "Token",
+        label: "Secteur d'activité",
+    },
+    {
+        name: "setting",
+        type: "Token",
+        label: "Cadre d'exercice",
+    },
 ]
